7f9222bd05eab667a980312ad8a11f943f1ae9c0
Dominic <44411887+dwc13@users.noreply.github.com>
2020-09-28 13:42:03 -0500

    Update logger.py
    Corrected ROS message format.

--- Path_logger/src/logger.py
+++ Path_logger/src/logger.py
@@ -7,23 +7,25 @@ from time import gmtime, strftime
 
 from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion,
 from nav_msgs.msg import Odometry
-from std_msgs.msg import UInt16, Header
+from std_msgs.msg import Header
 
 home = expanduser('~/Desktop/f1tenth_stuff/Path_logs')
 file = open(strftime(home+'/p-%Y-%m-%d-%H-%M-%S',gmtime())+'.csv', 'w')
 
 def save_posestamp(data):
     hdr = Header(stamp=rospy.Time.now(), frame_id='base_link')
-    quat = Quaternion(data.pose.pose.orientation.x, 
-                           data.pose.pose.orientation.y, 
-                           data.pose.pose.orientation.z, 
-                           data.pose.pose.orientation.w)
-    point = Point(data.twist.twist.linear.x, 
+    
+    posestamp = PoseStamp(header=hdr, pose=Pose(position=Point(
+                       data.twist.twist.linear.x, 
                        data.twist.twist.linear.y, 
-                       data.twist.twist.linear.z)
-                       
-    pose = Pose(point,quat)
-    posestamp = PoseStamp(hdr, pose)
+                       data.twist.twist.linear.z),
+                                                orientation=Quaternion(
+                       data.pose.pose.orientation.x, 
+                       data.pose.pose.orientation.y, 
+                       data.pose.pose.orientation.z, 
+                       data.pose.pose.orientation.w)
+                                               )
+                         )
   
     file.write('%f\n' % (posestamp)
 
